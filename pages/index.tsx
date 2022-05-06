56ffc3e9e34809fed16d318139e7cb9ef5755288
Yuya Takayanagi <59186280+yuyatakayanagi-cre@users.noreply.github.com>
2022-05-06 13:44:29 +0900

    リファクタ

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,35 @@ import { ApexChartsHorizontalBar } from "./charts/apexCharts/HorizontalBar";
 import { ChartjsHorizontalBar } from "./charts/chartjs/HorizontalBar";
 import { GoogleChartsHorizontalBar } from "./charts/googleCharts/HorizontalBar";
 
+const Line: React.VFC = () => {
+  return <Mui.Grid container spacing={4}></Mui.Grid>;
+};
+const Pie: React.VFC = () => {
+  return <Mui.Grid container spacing={4}></Mui.Grid>;
+};
+const HorizontalBar: React.VFC = () => {
+  return (
+    <Mui.Grid container spacing={4}>
+      <ApexChartsHorizontalBar />
+      <GoogleChartsHorizontalBar />
+      <ChartjsHorizontalBar />
+    </Mui.Grid>
+  );
+};
+
 const Covid19Prefectures: React.VFC = () => {
   const prefectures = useCovid19PrefecturesQuery();
   switch (prefectures.state) {
     case "hasValue":
       return (
-        <Mui.Grid container spacing={4}>
-          <ApexChartsHorizontalBar />
-          <GoogleChartsHorizontalBar />
-          <ChartjsHorizontalBar />
-        </Mui.Grid>
+        <>
+          横棒グラフ
+          <HorizontalBar />
+          円グラフ
+          <Pie />
+          折れ線グラフ
+          <Line />
+        </>
       );
     case "loading":
       return <Mui.CircularProgress />;
